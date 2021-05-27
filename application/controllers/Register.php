b43ecb9e71ec60562f0924da85f2e4986bc5f055
ferrylay <ferry.lay@student.umn.ac.id>
2021-05-27 16:18:39 +0800

    Login, Register

--- application/controllers/Register.php
+++ application/controllers/Register.php
@@ -3,8 +3,29 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Register extends CI_Controller {
 
+	function __construct()
+	{
+		parent::__construct();
+		$this->load->model("User_model");
+	}
+
 	public function index()
 	{
+		if($this->input->post('signUp')){
+			if($this->input->post('password') != $this->input->post('passwordconf')){
+				echo "Password != conf";exit;
+			}
+			$account = array(
+				'Fname' => $this->input->post('fname'),
+				'Lname' => $this->input->post('lname'),
+				'Email' => $this->input->post('email'),
+				'Password' => $this->input->post('password'), //md5($this->input->post('password')),
+				'BDay' => $this->input->post('bdate'),
+				'PNumber' => $this->input->post('pnumber')
+			);
+			$this->User_model->register($account);
+			redirect('login');
+		}
         $data['style'] = $this->load->view('include/style.php',NULL,TRUE);
         $data['script'] = $this->load->view('include/script.php',NULL,TRUE);
         
