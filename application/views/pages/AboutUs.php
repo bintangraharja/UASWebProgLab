81910d09087b6058a0fc6f11c560f43176d66242
AnandaSetiyani <ananda.setiyani@student.umn.ac.id>
2021-05-30 02:59:08 +0700

    Nan done 11 - aboutus

---
+++ application/views/pages/AboutUs.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Hotel UAS IF430 - XXX</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="style.css">
+
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+</head>
+<body>
+	<div class="container-fluid" style="background-color:#99E1D9; height: 50%; position: fixed;">
+		<div class="row d-flex justify-content-center" style="margin-top: 3%;">
+			<h1 style="color: white;">Meet Our Team Members</h1>
+		</div>
+		<div class="row">
+			<div class="col-2 d-flex justify-content-end">
+				<a href="HomePage.php">
+			        <button type="submit" class="btn btnBack"><span class="fas fa-angle-double-left"></span> Back</button>
+		        </a>
+			</div>
+		</div>
+	</div>
+	<div class="container-fluid" style="padding-top: 15%;">
+		<div class="row" data-aos="fade-up">
+            <div class="col-6 d-flex justify-content-end">
+                <img src="./Gallery/foto.jpg" style="width: 50%; border-radius: 50%;">
+            </div>
+            <div class="col-4 align-self-center" style="text-align : left;">
+                <h2 style="text-decoration: underline;">Ananda Setiyani</h2>
+                <h4>Front-end Developer - 00000037692</h4>
+                <a href="https://www.instagram.com/anandasf29/" target="_blank">
+                    <span class="fab fa-instagram fa-3x details"></span>
+                </a>
+                <a href="https://github.com/AnandaSetiyani" target="_blank">
+                    <span class="fab fa-github fa-3x details"></span>
+                </a>
+                <p style="text-align:justify;">“dipakein quotes biar keren, Lorem ipsum dolor sit amet, 
+                    consectetur adipiscing elit. Proin tincidunt dapibus ante 
+                    vel lobortis. Sed tempor cursus sodales. 
+                    Nulla sed porttitor lectus.”
+                </p>
+            </div>
+        </div>
+        <br><br>
+        <div class="row" data-aos="fade-up">
+            <div class="col-2"> </div>
+            <div class="col-4 align-self-center" style="text-align : right;">
+                <h2 style="text-decoration: underline;">Bintang Raharja</h2>
+                <h4>Back-end Developer - 0000003786</h4>
+                <a href="https://www.instagram.com/bintangraharja_/" target="_blank">
+                    <span class="fab fa-instagram fa-3x details"></span>
+                </a>
+                <a href="https://github.com/bintangraharja" target="_blank">
+                    <span class="fab fa-github fa-3x details"></span>
+                </a>
+                <p style="text-align:justify;">“dipakein quotes biar keren, Lorem ipsum dolor sit amet, 
+                    consectetur adipiscing elit. Proin tincidunt dapibus ante 
+                    vel lobortis. Sed tempor cursus sodales. 
+                    Nulla sed porttitor lectus.”
+                </p>
+            </div>
+            <div class="col-6">
+                <img src="./Gallery/foto.jpg" style="width: 50%; border-radius: 50%;">
+            </div>
+        </div>
+        <br><br>
+        <div class="row" data-aos="fade-up">
+            <div class="col-6  d-flex justify-content-end">
+                <img src="./Gallery/foto.jpg" style="width: 50%; border-radius: 50%;">
+            </div>
+            <div class="col-4 align-self-center" style="text-align : left;">
+                <h2 style="text-decoration: underline;">Ferry Lay</h2>
+                <h4>Back-end Developer - 00000037514</h4>
+                <a href="https://www.instagram.com/ferlay00/" target="_blank">
+                    <span class="fab fa-instagram fa-3x details"></span>
+                </a>
+                <a href="https://github.com/ferrylay" target="_blank">
+                    <span class="fab fa-github fa-3x details"></span>
+                </a>
+                <p style="text-align:justify;">“dipakein quotes biar keren, Lorem ipsum dolor sit amet, 
+                    consectetur adipiscing elit. Proin tincidunt dapibus ante 
+                    vel lobortis. Sed tempor cursus sodales. 
+                    Nulla sed porttitor lectus.”
+                </p>
+            </div>
+        </div>
+        <br><br>
+        <div class="row" data-aos="fade-up">
+            <div class="col-2"> </div>
+            <div class="col-4 align-self-center" style="text-align : right;">
+                <h2 style="text-decoration: underline;">Maureen Zerlina O.</h2>
+                <h4>Front-end Developer - 00000036850</h4>
+                <a href="https://www.instagram.com/maureenzerl/" target="_blank">
+                    <span class="fab fa-instagram fa-3x details"></span>
+                </a>
+                <a href="https://github.com/maureenzerl" target="_blank">
+                   <span class="fab fa-github fa-3x details"></span>
+                </a>
+                <p style="text-align:justify;">“dipakein quotes biar keren, Lorem ipsum dolor sit amet, 
+                    consectetur adipiscing elit. Proin tincidunt dapibus ante 
+                    vel lobortis. Sed tempor cursus sodales. 
+                    Nulla sed porttitor lectus.”
+                </p>
+            </div>
+            <div class="col-6">
+                <img src="./Gallery/foto.jpg" style="width: 50%; border-radius: 50%;">
+            </div>
+        </div>
+        <br><br>
+	</div>
+
+	<script>
+        AOS.init();
+    </script>
+</body>
+</html>
