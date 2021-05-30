e4295a260eaf9cd85e87f61b2df57b966231ce87
ferrylay <ferry.lay@student.umn.ac.id>
2021-05-30 12:46:32 +0800

    EditHotel

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -6,6 +6,7 @@ class Admin extends CI_Controller {
 	public function __construct(){
         parent::__construct();
         $this->load->model('home_model');
+        $this->load->model('admin_model');
     }
 	public function index()
 	{
@@ -19,5 +20,67 @@ class Admin extends CI_Controller {
         $data['sidebar'] = $this->load->view('sidebar/sidenavAdmin.php',$data,TRUE);
         $this->load->view('pages/homeadmin.php',$data);
 	}
+
+    public function editHotel(){
+        $id = $this->uri->segment(3);
+        if($this->input->post('updateHotelFacility')){
+            $MeetingRoom = 0;
+            $SwimmingPool = 0;
+            $Restaurant = 0;
+            $Spa = 0;
+            $Gym = 0;
+            $Bar = 0;
+            $WiFi = 0;
+            $Receptionist24 = 0;
+            $PUAirport = 0;
+            if($this->input->post('MeetingRoom')){
+                $MeetingRoom = 1;
+            }
+            if($this->input->post('SwimmingPool')){
+                $SwimmingPool = 1;
+            }
+            if($this->input->post('Restaurant')){
+                $Restaurant = 1;
+            }
+            if($this->input->post('Spa')){
+                $Spa = 1;
+            }
+            if($this->input->post('Gym')){
+                $Gym = 1;
+            }
+            if($this->input->post('Bar')){
+                $Bar = 1;
+            }
+            if($this->input->post('WiFi')){
+                $WiFi = 1;
+            }
+            if($this->input->post('Receptionist24')){
+                $Receptionist24 = 1;
+            }
+            if($this->input->post('PUAirport')){
+                $PUAirport = 1;
+            }
+            $values = array(
+                'MeetingRoom'   => $MeetingRoom,
+                'SwimmingPool'  => $SwimmingPool,
+                'Restaurant'    => $Restaurant,
+                'Spa'           => $Spa,
+                'Gym'           => $Gym,
+                'Bar'           => $Bar,
+                'WiFi'          => $WiFi,
+                'Receptionist24'=> $Receptionist24,
+                'PUAirport'     => $PUAirport
+            );
+            $this->admin_model->updateFacility($values,$id);
+            redirect('Admin/editHotel/'.$id);
+        }
+        $data['hotel'] = $this->admin_model->hotelDetail($id);
+        $data['facility'] = $this->admin_model->facilityDetail($id);
+        $data['room'] = $this->admin_model->roomDetail($id);
+        $data['style'] = $this->load->view('include/style.php',NULL,TRUE);
+        $data['script'] = $this->load->view('include/script.php',NULL,TRUE);
+        $data['sidebar'] = $this->load->view('sidebar/sidenavAdmin.php',$data,TRUE);
+        $this->load->view('pages/edithotel.php',$data);
+    }
 }
 ?>
--- application/views/pages/EditHotel.php
+++ application/views/pages/EditHotel.php
@@ -1,6 +1,5 @@
-<?php
-	include_once('sidenavAdmin.php');
-?>
+<?php echo $style;
+echo $script;?>
 
 <!DOCTYPE html>
 <html>
@@ -9,7 +8,7 @@
 		<div class="row">
 			<div class="col-3 d-flex justify-content-end">
 				<div class="d-flex align-items-end">
-					<a href="HomeAdmin.php"><button type="submit" class="btn btnBack btn-lg"><i class="fas fa-angle-double-left"></i> Back</button></a>
+					<a href="<?php echo site_url('Admin');?>"><button type="submit" class="btn btnBack btn-lg"><i class="fas fa-angle-double-left"></i> Back</button></a>
 				</div>
 			</div>
 			<div class="col">
@@ -40,25 +39,25 @@
 								<div class="form-group row">
 									<label class="col-3 col-form-label">Hotel's ID</label>
 									<div class="col-7">
-										<input type="text" class="form-control" name="hotelID" disabled>
+										<input type="text" value="<?= $hotel[0]['HotelID']?>" class="form-control" name="hotelID" disabled>
 									</div>
 								</div>
 								<div class="form-group row">
 									<label class="col-3 col-form-label">Hotel's Name</label>
 									<div class="col-7">
-										<input type="text" class="form-control" name="hotelName">
+										<input type="text" value="<?= $hotel[0]['HotelName']?>" class="form-control" name="hotelName">
 									</div>
 								</div>
 								<div class="form-group row">
 									<label class="col-3 col-form-label">Hotel's Rating</label>
 									<div class="col-7">
-										<input type="text" class="form-control" name="rating">
+										<input type="text" value="<?= $hotel[0]['Rating']?>" class="form-control" name="rating">
 									</div>
 								</div>
 								<div class="form-group row">
 									<label class="col-3 col-form-label">Hotel's Address</label>
 									<div class="col-7">
-										<textarea type="text" class="form-control" name="address"></textarea>
+										<textarea type="text" class="form-control" name="address"><?= $hotel[0]['Address']?></textarea>
 									</div>
 								</div>
 								<div class="form-group row">
@@ -80,50 +79,39 @@
 								<h5 class="col">Hotel's Main Facilities</h5>
 							</div>
                             <!-- edit facilities -->
-							<form action="">
+							<form action="" method="POST">
 								<div class="row" style="padding-top: 10px;">
 									<div class="col-4">
 										<div class="row" style="padding-left: 5%;">
 											<div class="form-check">
 												<label class="form-check-label">
-												<input type="checkbox" class="form-check-input" value="">Meeting Room
+												<input type="checkbox" name="MeetingRoom" class="form-check-input" value="1" 
+												<?php if($facility[0]["MeetingRoom"] != 0){
+														echo "checked";
+													}
+												?>>Meeting Room
 												</label>
 											</div>
 										</div>
 										<div class="row" style="padding-left: 5%;">
 											<div class="form-check">
 												<label class="form-check-label">
-												<input type="checkbox" class="form-check-input" value="">Swimming Pool
+												<input type="checkbox" name="SwimmingPool" class="form-check-input" value="1"
+												<?php if($facility[0]["SwimmingPool"] != 0){
+														echo "checked";
+													}
+												?>>Swimming Pool
 												</label>
 											</div>
 										</div>
 										<div class="row" style="padding-left: 5%;">
 											<div class="form-check">
 												<label class="form-check-label">
-												<input type="checkbox" class="form-check-input" value="">Restaurant
-												</label>
-											</div>
-										</div>
-									</div>
-									<div class="col-4">
-										<div class="row" style="padding-left: 5%;">
-											<div class="form-check">
-												<label class="form-check-label">
-												<input type="checkbox" class="form-check-input" value="">Receptionist 24 Hours
-												</label>
-											</div>
-										</div>
-										<div class="row" style="padding-left: 5%;">
-											<div class="form-check">
-												<label class="form-check-label">
-												<input type="checkbox" class="form-check-input" value="">WiFi
-												</label>
-											</div>
-										</div>
-										<div class="row" style="padding-left: 5%;">
-											<div class="form-check">
-												<label class="form-check-label">
-												<input type="checkbox" class="form-check-input" value="">Spa
+												<input type="checkbox" name="Restaurant" class="form-check-input" value="1"
+												<?php if($facility[0]["Restaurant"] != 0){
+														echo "checked";
+													}
+												?>>Restaurant
 												</label>
 											</div>
 										</div>
@@ -132,28 +120,75 @@
 										<div class="row" style="padding-left: 5%;">
 											<div class="form-check">
 												<label class="form-check-label">
-												<input type="checkbox" class="form-check-input" value="">Gym
+												<input type="checkbox" name="Receptionist24" class="form-check-input" value="1"
+												<?php if($facility[0]["Receptionist24"] != 0){
+														echo "checked";
+													}
+												?>>Receptionist 24 Hours
 												</label>
 											</div>
 										</div>
 										<div class="row" style="padding-left: 5%;">
 											<div class="form-check">
 												<label class="form-check-label">
-												<input type="checkbox" class="form-check-input" value="">Bar
+												<input type="checkbox" name="WiFi" class="form-check-input" value="1"
+												<?php if($facility[0]["WiFi"] != 0){
+														echo "checked";
+													}
+												?>>WiFi
 												</label>
 											</div>
 										</div>
 										<div class="row" style="padding-left: 5%;">
 											<div class="form-check">
 												<label class="form-check-label">
-												<input type="checkbox" class="form-check-input" value="">Picked Up from Airport
+												<input type="checkbox" name="Spa" class="form-check-input" value="1"
+												<?php if($facility[0]["Spa"] != 0){
+														echo "checked";
+													}
+												?>>Spa
+												</label>
+											</div>
+										</div>
+									</div>
+									<div class="col-4">
+										<div class="row" style="padding-left: 5%;">
+											<div class="form-check">
+												<label class="form-check-label">
+												<input type="checkbox" name="Gym" class="form-check-input" value="1"
+												<?php if($facility[0]["Gym"] != 0){
+														echo "checked";
+													}
+												?>>Gym
+												</label>
+											</div>
+										</div>
+										<div class="row" style="padding-left: 5%;">
+											<div class="form-check">
+												<label class="form-check-label">
+												<input type="checkbox" name="Bar" class="form-check-input" value="1"
+												<?php if($facility[0]["Bar"] != 0){
+														echo "checked";
+													}
+												?>>Bar
+												</label>
+											</div>
+										</div>
+										<div class="row" style="padding-left: 5%;">
+											<div class="form-check">
+												<label class="form-check-label">
+												<input type="checkbox" name="PUAirport" class="form-check-input" value="1"
+												<?php if($facility[0]["PUAirport"] != 0){
+														echo "checked";
+													}
+												?>>Picked Up from Airport
 												</label>
 											</div>
 										</div>
 									</div>
 								</div>
 								<div class="row d-flex justify-content-end" style="padding-right: 15px;">
-									<input type="submit" class="btn btnYes" name="updateHotel" value="UPDATE">
+									<input type="submit" class="btn btnYes" name="updateHotelFacility" value="UPDATE">
 								</div>
 							</form>
 						</div>
@@ -167,10 +202,10 @@
 							</div>
 							<div class="row">
 								<div class="col-3">
-									<img src="./Gallery/H0003 - 2.jpg" style="width: 125px; height: 75px;">
+									<img src="<?php echo site_url('DetailHotel/hotelImg/').$hotel[0]['HotelID'].'/1'?>" style="width: 125px; height: 75px;">
 								</div>
 								<div class="col-6 align-self-center">
-									<h4 class="text-center">Image's ID</h4>
+									<h4 class="text-center">1</h4>
 								</div>
 								<div class="col-3 align-self-center">
 									<a id="editPhoto"><button class="btn btnYes">Edit Photo</button></a>
@@ -179,10 +214,10 @@
 							<hr style="border-color: black;">
 							<div class="row">
 								<div class="col-3">
-									<img src="./Gallery/H0003 - 2.jpg" style="width: 125px; height: 75px;">
+									<img src="<?php echo site_url('DetailHotel/hotelImg/').$hotel[0]['HotelID'].'/2'?>" style="width: 125px; height: 75px;">
 								</div>
 								<div class="col-6 align-self-center">
-									<h4 class="text-center">Image's ID</h4>
+									<h4 class="text-center">2</h4>
 								</div>
 								<div class="col-3 align-self-center">
 									<a id="editPhoto"><button class="btn btnYes">Edit Photo</button></a>
@@ -191,10 +226,10 @@
 							<hr style="border-color: black;">
 							<div class="row">
 								<div class="col-3">
-									<img src="./Gallery/H0003 - 2.jpg" style="width: 125px; height: 75px;">
+									<img src="<?php echo site_url('DetailHotel/hotelImg/').$hotel[0]['HotelID'].'/3'?>" style="width: 125px; height: 75px;">
 								</div>
 								<div class="col-6 align-self-center">
-									<h4 class="text-center">Image's ID</h4>
+									<h4 class="text-center">3</h4>
 								</div>
 								<div class="col-3 align-self-center">
 									<a id="editPhoto"><button class="btn btnYes">Edit Photo</button></a>
@@ -244,77 +279,31 @@
 					<hr style="background-color: white;">
 					<div class="row">
 						<div class="col custom-scrollbar-css p-2" style="height: 250px;">
+							<?php foreach($room as $row){
+									
+								?>
 							<div class="row">
 								<div class="col-2 text-center">
-									<img src="./Gallery/R4001.jpg" style="width: 125px; height:85px;">
+									<img src="<?php echo site_url('DetailHotel/roomImg/').$hotel[0]['HotelID'].'/'.$row['RoomID'];?>" style="width: 125px; height:85px;">
 								</div>
 								<div class="col-2 text-center">
-									<p>R4001</p>
+									<p><?= $row['RoomID']?></p>
 								</div>
 								<div class="col-2 text-center">
-									<p>Premiere Deluxe Room</p>
+									<p><?= $row['RoomName']?></p>
 								</div>
 								<div class="col-2 text-center">
-									<p>Rp 800.000,-</p>
+									<p>Rp <?= $row['Price']?>,-</p>
 								</div>
 								<div class="col-2">
-									<li>Breakfast (2 pax)</li>
-									<li>Double Bed</li>
-									<li>2 Guest</li>
-									<li>32m<sup>2</sup></li>
+									<?= $row['Facility']?>
 								</div>
 								<div class="col-2 text-center">
 									<a id="editRoom"><button class="btn btnYes">Edit Room</button></a>
 								</div>
 							</div>
 							<hr style="border-color: black;">
-							<div class="row">
-								<div class="col-2 text-center">
-									<img src="./Gallery/R4001.jpg" style="width: 125px; height:85px;">
-								</div>
-								<div class="col-2 text-center">
-									<p>R4001</p>
-								</div>
-								<div class="col-2 text-center">
-									<p>Premiere Deluxe Room</p>
-								</div>
-								<div class="col-2 text-center">
-									<p>Rp 800.000,-</p>
-								</div>
-								<div class="col-2">
-									<li>Breakfast (2 pax)</li>
-									<li>Double Bed</li>
-									<li>2 Guest</li>
-									<li>32m<sup>2</sup></li>
-								</div>
-								<div class="col-2 text-center">
-									<a id="editRoom"><button class="btn btnYes">Edit Room</button></a>
-								</div>
-							</div>
-							<hr style="border-color: black;">
-							<div class="row">
-								<div class="col-2 text-center">
-									<img src="./Gallery/R4001.jpg" style="width: 125px; height:85px;">
-								</div>
-								<div class="col-2 text-center">
-									<p>R4001</p>
-								</div>
-								<div class="col-2 text-center">
-									<p>Premiere Deluxe Room</p>
-								</div>
-								<div class="col-2 text-center">
-									<p>Rp 800.000,-</p>
-								</div>
-								<div class="col-2">
-									<li>Breakfast (2 pax)</li>
-									<li>Double Bed</li>
-									<li>2 Guest</li>
-									<li>32m<sup>2</sup></li>
-								</div>
-								<div class="col-2 text-center">
-									<a id="editRoom"><button class="btn btnYes">Edit Room</button></a>
-								</div>
-							</div>
+							<?php }?>
 						</div>
 					</div>
 				</div>
